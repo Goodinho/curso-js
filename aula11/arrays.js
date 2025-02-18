0324ab97aec67c8647679986f48139ab8c14b3e2
Eduardo Godinho <eduardohgds4@gmail.com>
2025-02-18 16:31:10 -0300

    Conteúdo da aula 11

---
+++ aula11/arrays.js
@@ -0,0 +1,17 @@
+const alunos = ['Luiz', 'Eduardo', 'João'];
+console.log(alunos);
+
+// console.log(alunos instanceof Array); verifica se é um Array ou não 
+//console.log(alunos.slice(0,-4)); Fatiando um array
+//delete alunos[0]; Remove um elemento sem fazer com que os outros saiam do lugar 
+//alunos.shift(); Remove um elemento do começo 
+//const removido = alunos.pop(); Consegue salvar em uma váriavel
+//alunos.pop(); remove um elemento do final 
+//alunos.unshift('Ferdinando'); Coloca no começo 
+//alunos.unshift('Maria'); Coloca no começo 
+//alunos.push('joaquim'); Usando a função "push" coloca um elemento no final do array 
+//alunos.push('Miguel'); Usando a função "push" coloca um elemento no final do array 
+//alunos[alunos.length] = 'Pedro'; coloca um elemento no final do array 
+//alunos[alunos.length] = 'Fábio'; coloca um elemento no final do array 
+//alunos[alunos.length] = 'Trigo'; coloca um elemento no final do array 
+
