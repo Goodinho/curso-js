528eb94e641f25cf29ebf64ba7629f95a124ee68
Eduardo Godinho <eduardohgds4@gmail.com>
2025-03-25 20:59:10 -0300

    Aprendi switch/case

--- aula24/SwitchCase.js
+++ aula24/SwitchCase.js
@@ -1,3 +1,88 @@
-const data = new Date()
-const diaSemana = data.getDay()
-console.log(data.toLocaleDateString("pt-br", {weekday:"long"}))
+/*
+const data = new Date('1987-04-06 00:00:00');
+let diaSemana = data.getDay();
+let diaSemanaTexto;
+
+switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = "Domingo";
+        
+        break;
+    
+        case 1:
+        diaSemanaTexto = "Segunda";
+        
+        break;
+
+        case 2:
+        diaSemanaTexto = "Terça";
+        
+        break;
+
+        case 3:
+        diaSemanaTexto = "Quarta";
+        
+        break;
+
+        case 4:
+        diaSemanaTexto = "Quinta";
+        break;
+
+        case 5:
+        diaSemanaTexto = "Sexta";
+        
+        break;
+
+        case 6:
+        diaSemanaTexto = "Sábado";
+        
+        break;
+    default:
+        break;
+}
+console.log(diaSemana, diaSemanaTexto);
+*/
+function getDiaSemanaTexto(diaSemana){
+    let diaSemanaTexto; 
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = "Domingo";
+            
+            return diaSemanaTexto;
+        
+            case 1:
+            diaSemanaTexto = "Segunda";
+            
+            return diaSemanaTexto;
+    
+            case 2:
+            diaSemanaTexto = "Terça";
+            
+            return diaSemanaTexto;
+    
+            case 3:
+            diaSemanaTexto = "Quarta";
+            
+            return diaSemanaTexto;
+    
+            case 4:
+            diaSemana = "Quinta";
+            return diaSemanaTexto;
+    
+            case 5:
+            diaSemanaTexto = "Sexta";
+            
+            return diaSemanaTexto;
+    
+            case 6:
+            diaSemanaTexto = "Sábado";
+            
+            return diaSemanaTexto;
+       
+}
+}
+const data = new Date('1987-04-06 00:00:00');
+let diaSemana = data.getDay();
+
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+console.log(diaSemana, diaSemanaTexto);
