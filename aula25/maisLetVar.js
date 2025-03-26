a2a5a9ed98fa13e87fa2b4e858ccf987748f722a
Eduardo Godinho <eduardohgds4@gmail.com>
2025-03-26 19:52:41 -0300

    Criei a aula25

---
+++ aula25/maisLetVar.js
@@ -0,0 +1,24 @@
+const verdadeira = true; 
+
+//Let tem escopo de bloco {... bloco}
+// Var só tem escopo de função 
+
+// let nome = 'Luiz'; // criando 
+// var nome2 = 'luiz';
+
+// if (verdadeira) {
+//     let nome = 'Otávio' // criando
+//     var nome2 = 'Rogério' // redeclarando
+
+//     if (verdadeira){
+//         let nome = 'Outra coisa '; //criando
+//         var nome2 = 'Ronaldo' // redeclarando
+//         console.log(nome);
+//     }
+     
+// }
+
+// console.log(nome, nome2);
+console.log(sobrenome);
+var sobrenome = 'Godinho';
+
